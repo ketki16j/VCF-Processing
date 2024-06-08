79f42d2bfc972b5aee2f42f580f763b229e56d58
ketkij16 <ketkijoshi1996@gmail.com>
2024-06-08 11:07:34 -0500

    fourth changes

--- vcf_processing.sh
+++ vcf_processing.sh
@@ -7,4 +7,13 @@ for chr in $(seq 1 22); do
            --out /myData.chr$chr ;
 done
 
-
+##for each chr vcf update the column ID with dbSNP identifier and format to get only the genotype information from the vcf:
+for chr in $(seq 1 2);do
+bcftools annotate -x ID chr${chr}.vcf > chr${chr}_noid.vcf  ##removes id
+## now annotate dbSNP "rsid" in ID column for vcf files
+bgzip chr${chr}_noid.vcf
+tabix -p vcf chr${chr}_noid.vcf.gz
+java -jar SnpSift.jar annotate 00-All.vcf chr${chr}_noid.vcf.gz > chr${chr}.vcf
+## one annotated with rsid now format it to keep only GT information
+bcftools annotate -x FORMAT chr${chr}_annotate.vcf > chr${chr}_en.vcf
+done
