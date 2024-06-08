ad78b02b47d44f076f4e6c75172a2c898c57efbe
ketki16j <ketkijoshi1996@gmail.com>
2024-06-08 10:43:15 -0500

    third changes

--- vcf_processing.sh
+++ vcf_processing.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ##extract individual chromosome vcf from the whole genome vcf
-for i in 1:22
-do
+for chr in $(seq 1 22); do
+     plink --vcf GTEx_Analysis_2017-06-05_v8_WholeExomeSeq_979Indiv_VEP_annot.vcf.gz \
+           --chr $chr \
+           --recode \
+           --out /myData.chr$chr ;
 done
+
 
