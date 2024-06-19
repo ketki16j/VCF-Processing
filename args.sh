58ba0d37bdc45b5e8c2ca66d9519213ffb850f75
ketkij16 <ketkijoshi1996@gmail.com>
2024-06-19 12:30:42 -0500

    removed files

--- args.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-##pass arguments to the script
-
-echo $1 $2 $3
-echo "argument passed by users are $1 $2 $3"
-
-#pass arguments to the script in form of an array:
-args=("$@")
-echo "arguments passed by user are ${args[1]}, ${args[2]}"
